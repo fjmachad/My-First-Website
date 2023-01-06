2228af4d3f99b1a59f22e8d0ecfe45769f7376c6
fjmachad <fjmachado11@gmail.com>
2023-01-06 16:17:03 -0500

    add comments to javascript file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var header = document.getElementById("name");
+//If color is crimson then the header will be in original state
+//flip to green and increase the font size
+// if its not crimson then it has been clicked and we revert back to its original state
 function changeColor(event){
     if (event.target.style.color === "crimson"){
         event.target.style.color = "green";
